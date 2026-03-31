5e4073f188ce116b2ca575cb15961cf9b78770f4
unknown <harshita.tantia1@gmail.com>
2026-03-31 23:01:31 +0530

    Initial commit

---
+++ client/ai-resume-analyzer/src/App.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [resume, setResume] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
+  const [fileData, setFileData] = useState(null);
+  const [prompt, setPrompt] = useState("");
+
+  async function handleSubmit(type) {
+    if(!resume && ! fileData) return;
+    setLoading(true);
+
+    const formdata = new FormData();
+    if(resume) {
+       formdata.append("resumeText", resume);
+    }
+
+    formdata.append("promptType", type);
+    formdata.append("customPrompt", prompt);
+
+    if(fileData) {
+      formdata.append("resume", fileData);
+    }
+
+    try {
+      const request = await axios.post("http://localhost:5000/analyze", 
+        formdata,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setResponse(request.data.result);
+    } catch (err) {
+      console.log(err.response?.data); 
+      setResponse("Something went wrong");
+    }
+    setLoading(false);
+  }
+
+  async function testAPI() {
+    const res = await axios.get("http://localhost:5000");
+    console.log(res.data);
+  }
+
+  useEffect(() => {
+    testAPI();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>AI Resume Analyzer</h1>
+        <div>
+        <textarea
+          placeholder="Paste your resume..."
+          value={resume}
+          onChange={(e) => setResume(e.target.value)}
+        />
+
+        <textarea
+          placeholder="Write your custom prompt"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <input type = "file" accept = ".pdf" onChange = {(e) => setFileData(e.target.files[0])}/>
+         <button onClick={() => handleSubmit("send")}>
+            Send
+          </button>
+        </div>
+
+        <div className="buttons">
+          <button onClick={() => handleSubmit("improve")}>
+            Improve my resume
+          </button>
+          <button onClick={() => handleSubmit("ats")}>
+            What is the ATS score of my resume
+          </button>
+          <button onClick={() => handleSubmit("analyze")}>
+            Do a proper analysis of my resume
+          </button>
+          <button onClick={() => handleSubmit("talk")}>
+            Give me summary of my resume
+          </button>
+        </div>
+
+        {loading ? <p>Loading...</p> : <div className="result">{response}</div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//input text box for custom prompt
+//attach resume feature
+//initial screen: either show two buttons, one paste resume another attach resume
+//if i click paste then the text area appears, if i click attach file explorer opens
+//after that i can click any of the prompt buttons or write own custom prompt and click send
+//then the result will be shown below. 
---
+++ server/index.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const Groq = require("groq-sdk");
+const multer = require("multer");
+const pdfParse = require("pdf-parse");
+console.log("pdfParse type:", typeof pdfParse);
+console.log("pdfParse value:", pdfParse);
+const PORT = 5000;
+
+dotenv.config();
+const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 5 * 1024 * 1024 }});
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//GROQ setup
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+//main AI route
+app.post("/analyze", upload.single("resume"), async (req, res) => {
+  try {
+    let resumeText = req.body.resumeText;
+    const { promptType, customPrompt} = req.body;
+
+    if(req.file) {
+      console.log("File received:", req.file.originalname);
+       try {
+    const pdfData = await pdfParse(req.file.buffer);
+    console.log("Extracted text length:", pdfData.text.length); // 👈 ADD
+    resumeText = pdfData.text;
+  } catch (err) {
+    console.error("PDF Parse Error:", err); // 👈 ADD
+    return res.status(400).json({
+      error: "Failed to read PDF file",
+    });
+  }
+    }
+
+    if (!resumeText) {
+      return res.status(400).json({
+        error: "Resume Text is required",
+      });
+    }
+
+    let prompt = "";
+
+    if (promptType === "improve") {
+      prompt = `
+You are a professional resume writer.
+
+Improve the following resume:
+- Use strong action verbs
+- Make it concise
+- Highlight achievements
+
+Resume:
+${resumeText} 
+${customPrompt}
+`;
+    } else if (promptType === "ats") {
+      prompt = `Analyze this resume for ATS compatibility.
+
+Return:
+- ATS score out of 100
+- Missing keywords
+- Formatting issues
+- Suggestions
+
+Return response in JSON format:
+
+{
+  "atsScore": number,
+  "strengths": [],
+  "weaknesses": [],
+  "suggestions": []
+}
+
+Resume:
+${resumeText} 
+${customPrompt}
+`;
+
+    } else if (promptType === "analyze") {
+      prompt = `
+Act as a hiring manager.
+
+Analyze this resume and provide:
+- Strengths
+- Weaknesses
+- Missing skills
+- Suggestions to improve
+
+Resume:
+${resumeText} 
+${customPrompt}
+`;
+    } else {
+      prompt = `${resumeText} 
+${customPrompt}
+` ;
+    }
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.1-8b-instant",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+
+    res.json({
+      result: completion.choices[0].message.content,
+    });
+  } catch (err) {
+    console.error("Error: ", err);
+
+    res.status(500).json({
+      error: "Something went wrong",
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+  console.log("API KEY:", process.env.GROQ_API_KEY);
+});
+
+app.use((err, req, res, next) => {
+  console.error("MULTER ERROR:", err);
+
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  res.status(500).json({
+    error: err.message || "Something went wrong",
+  });
+});
